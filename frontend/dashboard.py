c1d94d5422dc02afe38d5f711159b9dfeadb7880
Aaron Tang <tangnaaron.work@gmail.com>
2025-08-31 14:43:51 -0700

    Dashboard prediction building

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -1,18 +1,21 @@
 import pandas as pd 
 import sys
-#sys.path.append('/Users/aarontang/Desktop/Projects/Video Game Rating Forecast ')
-#from backend.model import init_model, model_predict
+sys.path.append('/Users/aarontang/Desktop/Projects/Video Game Rating Forecast ')
+from backend.model import init_model, model_predict
 import plotly.express as px
-from dash import Dash, html, dcc, callback
+import plotly.graph_objects as go
+from dash import Dash, html, dcc, callback, ctx
 from dash.dependencies import Input, Output
 
+# Imports mapping
+genre_df = pd.read_csv("mapping/genre_mapping.csv")
+theme_df = pd.read_csv("mapping/theme_mapping.csv")
+
 # Initialize forecast model 
-#model = init_model()
-#print(model_predict(['Puzzle'], ['Drama'],"Electronic Arts", model))
+model = init_model()
 
 # Create dashboard 
 games_cleaned = pd.read_csv("csv/games_cleaned.csv")
-#games_cleaned = pd.read_csv("/Users/aarontang/Desktop/Projects/Video Game Rating Forecast /csv/games_cleaned.csv")
 
 def create_chart(company):
     df = games_cleaned[games_cleaned['company'] == company]
@@ -61,14 +64,18 @@ app.layout = html.Div([
         html.Div([
             html.Label("Genre:"),
             dcc.Dropdown(id = 'genre', 
-                         value = "placeholder1",
-                         placeholder = "Select genre(s):"),
+                         options = genre_df['name'].to_list(),
+                         placeholder = "Select genre(s):",
+                         multi = True,
+                         closeOnSelect = False),
         ], style = {'width' : '30%', 'padding-right' : '10px'}),
         html.Div([
             html.Label("Theme:"),
             dcc.Dropdown(id = 'theme', 
-                         value = "placeholder2",
-                         placeholder = "Select theme(s):")
+                         options = theme_df['name'].to_list(),
+                         placeholder = "Select theme(s):",
+                         multi = True,
+                         closeOnSelect = False)
         ], style = {'width' : '30%', 'padding-left' : '10px', 'padding-right' : '25px'}),
         html.Div([
             html.Br(),
@@ -81,15 +88,39 @@ app.layout = html.Div([
                                  'alignItems' : 'center', 'textAlign' : 'center',
                                  'padding-top' : '10px', 'justifyContent' : 'center'})
         ])
-    ], style = {'display' : 'flex', 'justifyContent' : 'center'})
+    ], style = {'display' : 'flex', 'justifyContent' : 'center'}),
+    html.Br(),
+    html.Br(),
+    html.Br()
 ])
 
 # Updates webpage
 @callback(Output('bar_chart', 'figure'),
           Input('company', 'value'),
+          Input('genre', 'value'),
+          Input('theme', 'value'),
+          Input('predict', 'n_clicks'),
           prevent_initial_call = True)
-def update_chart(company):
-    return create_chart(company)
+def update_chart_with_prediction(company, genre, theme, predict):
+    if company != None:
+        if "predict" == ctx.triggered_id:
+            if (genre != None) and (theme != None) and (len(genre) != 0) and (len(theme) != 0):
+                # Creates prediction
+                prediction = model_predict(genre, theme, company, model)
+                
+                # Graphs prediction with chart 
+                fig = create_chart(company)
+                fig.add_trace(go.Bar(x = ["New Game"], y = [prediction], 
+                                     marker_color = 'green',
+                                     hovertext = f"The predicted rating for the new game is {prediction}",
+                                     showlegend = False))
+                return fig
+            else:
+                return create_chart(company)
+        else:
+             return create_chart(company)
+    else:
+        return create_chart(company = "Electronic Arts")
 
 # Runs dashboard website
 if __name__ == '__main__':

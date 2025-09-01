03e9a09c36658331cfc9af0854af04aeb68c85bb
Aaron Tang <tangnaaron.work@gmail.com>
2025-09-01 15:15:04 -0700

    Workflow test

--- backend/data_backfill.py
+++ backend/data_backfill.py
@@ -1,62 +1,57 @@
 from igdb.wrapper import IGDBWrapper
 import pandas as pd
 import warnings
-import time
 from requests.exceptions import HTTPError
 from data_cleaning import transform_data
 
 # Disables FutureWarning
 warnings.simplefilter(action = 'ignore', category = FutureWarning)
 
-# Environnment variables 
-client_id = 'w7r7cmzkm0etx41ula5aq00oatzc4c'
-token = 'q3ax0dsj0s3565jvyikqu7kfx90elr'
+def data_backfill(company_list, client_id, token):
+    # Get company's published games 
+    def get_games_df(company):
+        wrapper = IGDBWrapper(client_id, token)
+        
+        games_df = pd.DataFrame()
+        company_request = wrapper.api_request(
+                    'companies',
+                    f'fields developed, updated_at, name; where name = "{company}";'
+                )
+        company_json = company_request.decode('utf-8')
 
-# Get company's published games 
-def get_games_df(company):
-    wrapper = IGDBWrapper(client_id, token)
-    
+        company_df = pd.read_json(company_json)
+        company_published_games_id = company_df.loc[0, 'developed']
+        
+        try:
+        
+            column_request = wrapper.api_request(
+                    'games',
+                    f'fields name, genres, themes, total_rating; limit 500; where id = \
+                    {tuple(company_published_games_id)};'
+                )
+            column_json = column_request.decode('utf-8')
+            games_df = pd.read_json(column_json)
+
+                
+            games_df['company'] = company 
+            games_df['updated_at'] = company_df.loc[0,'updated_at']
+
+            return games_df
+        
+        except HTTPError:
+            print("Too many requests for " + company)
+            return 
+        
     games_df = pd.DataFrame()
-    company_request = wrapper.api_request(
-                'companies',
-                f'fields developed, updated_at, name; where name = "{company}";'
-            )
-    company_json = company_request.decode('utf-8')
 
-    company_df = pd.read_json(company_json)
-    company_published_games_id = company_df.loc[0, 'developed']
-    
-    try:
-    
-        column_request = wrapper.api_request(
-                'games',
-                f'fields name, genres, themes, total_rating; limit 500; where id = \
-                {tuple(company_published_games_id)};'
-            )
-        column_json = column_request.decode('utf-8')
-        games_df = pd.read_json(column_json)
+    # Aggregates company game dataset
+    for company in company_list:
+        games_df = pd.concat([games_df, get_games_df(company)])
 
-            
-        games_df['company'] = company 
-        games_df['updated_at'] = company_df.loc[0,'updated_at']
+    # Cleans company game dataset
+    games_df.set_index('id', inplace = True)
+    games_df = transform_data(games_df)
 
-        return games_df
-    
-    except HTTPError:
-        print("Too many requests for " + company)
-        return 
-    
-companies = ["Electronic Arts", "Riot Games", "Square Enix", "Epic Games", "Nintendo"]
-games_df = pd.DataFrame()
-
-# Aggregates company game dataset
-for company in companies:
-    games_df = pd.concat([games_df, get_games_df(company)])
-
-# Cleans company game dataset
-games_df.set_index('id', inplace = True)
-games_df = transform_data(games_df)
-
-# Exports 
-games_df.to_csv("csv/games_cleaned.csv", index = False)
+    # Exports 
+    games_df.to_csv("csv/games_cleaned1.csv", index = False)
 
---
+++ backend/data_refresh.py
@@ -0,0 +1,39 @@
+from igdb.wrapper import IGDBWrapper
+import pandas as pd
+import warnings
+from data_backfill import data_backfill
+from requests import post
+
+# Disables FutureWarning
+warnings.simplefilter(action = 'ignore', category = FutureWarning)
+
+# API variables 
+response = post('https://id.twitch.tv/oauth2/token?client_id=w7r7cmzkm0etx41ula5aq00oatzc4c&client_secret=u7jjy34lwqjddnms6ini0il27gfg13&grant_type=client_credentials')
+
+client_id = 'w7r7cmzkm0etx41ula5aq00oatzc4c'
+token = response.json()['access_token']
+companies = ["Electronic Arts", "Riot Games", "Square Enix", "Epic Games", "Nintendo"]
+
+# Obtains new company data 
+company_df = pd.DataFrame()
+games_cleaned = pd.read_csv("csv/games_cleaned.csv")
+for company in companies:
+    wrapper = IGDBWrapper(client_id, token)
+            
+    company_request = wrapper.api_request(
+                'companies',
+                f'fields updated_at, name; where name = "{company}";'
+            )
+    company_json = company_request.decode('utf-8')
+
+    company_df = pd.concat([company_df, pd.read_json(company_json)], ignore_index = True)
+
+
+games_cleaned['updated_at'] = pd.to_datetime(games_cleaned['updated_at'])
+
+# Data refresh 
+if games_cleaned.groupby('company')['updated_at'].max().to_list() < company_df['updated_at'].to_list():
+    data_backfill(companies, client_id, token)
+    print("New data loaded.")
+else:
+    print("No new data.")

90df0879caa55a50b85dfb85497d28da142584dc
Aaron Tang <tangnaaron.work@gmail.com>
2025-09-02 15:57:34 -0700

    Automation fixing pt3

--- backend/data_refresh.sh
+++ backend/data_refresh.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+pip install igdb
+pip install pandas 
+
 python backend/data_refresh.py
 echo "Running"
 
 p=$(pwd)
 git config --global --add safe.directory $p
 
-if [["$(git status --porcelain)" != ""]]; then 
+if ["$(git status --porcelain)" != ""]; then 
     git config --global user.name $USER_NAME
     git config --global user.email $USER_EMAIL
     git add csv/games_cleaned1.csv

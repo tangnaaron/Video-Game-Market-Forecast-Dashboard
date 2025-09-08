5d78990bf8ad710a310fa971e72c1c1d2cc40db5
tangnaaron <tangnaaron.work@gmail.com>
2025-09-08 14:00:15 -0700

    Update data_cleaning.py

--- backend/data_cleaning.py
+++ backend/data_cleaning.py
@@ -1,21 +1,7 @@
 from igdb.wrapper import IGDBWrapper
 import pandas as pd
 from ast import literal_eval
-'''
-client_id = 'w7r7cmzkm0etx41ula5aq00oatzc4c'
-token = 'q3ax0dsj0s3565jvyikqu7kfx90elr'
 
-wrapper = IGDBWrapper(client_id, token)
-
-theme_request = wrapper.api_request(
-                'genres',
-                f'fields name; limit 500;'
-            )
-
-json = theme_request.decode('utf-8')
-df = pd.read_json(json)
-df.to_csv("mapping/genre_mapping.csv", index = False)
-'''
 
 def transform_data(df):
     genre_mapping = pd.read_csv("mapping/genre_mapping.csv")

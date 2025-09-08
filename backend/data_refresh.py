31df43941d0719611ee6fc7c66121d862434599e
tangnaaron <tangnaaron.work@gmail.com>
2025-09-08 15:03:55 -0700

    Update data_refresh.py

--- backend/data_refresh.py
+++ backend/data_refresh.py
@@ -9,9 +9,9 @@ import os
 warnings.simplefilter(action = 'ignore', category = FutureWarning)
 
 # API variables 
-client_secret = os.getenv('MY_CLIENT_SECRET')
-#print(client_secret)
-response = post('https://id.twitch.tv/oauth2/token?client_id=w7r7cmzkm0etx41ula5aq00oatzc4c&client_secret=u7jjy34lwqjddnms6ini0il27gfg13&grant_type=client_credentials')
+client_secret = str(os.getenv('MY_CLIENT_SECRET'))
+#response = post('https://id.twitch.tv/oauth2/token?client_id=w7r7cmzkm0etx41ula5aq00oatzc4c&client_secret=u7jjy34lwqjddnms6ini0il27gfg13&grant_type=client_credentials')
+response = post(f'https://id.twitch.tv/oauth2/token?client_id=w7r7cmzkm0etx41ula5aq00oatzc4c&client_secret={client_secret}&grant_type=client_credentials')
 client_id = 'w7r7cmzkm0etx41ula5aq00oatzc4c'
 token = response.json()['access_token']
 companies = ["Electronic Arts", "Riot Games", "Square Enix", "Epic Games", "Nintendo"]

3601b0d41e05e0a144e6320f060c1eb4e410674e
tangnaaron <tangnaaron.work@gmail.com>
2025-09-08 19:46:30 -0700

    Update data_refresh.sh

--- backend/data_refresh.sh
+++ backend/data_refresh.sh
@@ -2,7 +2,7 @@
 pip --no-cache-dir install --root-user-action=ignore igdb-api-v4
 pip --no-cache-dir install --root-user-action=ignore pandas 
 
-export CLIENT_SECRET=$CLIENT_SECRET
+export CLIENT_SECRET="$CLIENT_SECRET"
 python backend/data_refresh.py
 echo "Running"
 
